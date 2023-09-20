696572ab7aab9ad89207e34d88ed0ca967e8111a
Jean Pierre <jeanpierre101298@gmail.com>
2023-09-20 14:32:03 -0500

    Fix/handle error if the url parser cannot read the encoded url

--- src/store/useEditorValues.ts
+++ src/store/useEditorValues.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { url } from "../utils/urlHelper";
 import { EditorValues } from "../types/index.types";
+import { useSnackbarStore } from "./useSnackbarStore";
 
 type EditorValuesState = {
   values: EditorValues;
@@ -8,7 +9,18 @@ type EditorValuesState = {
 };
 
 export const useEditorValuesStore = create<EditorValuesState>((set) => ({
-  values: url.getDecodedValues(),
+  values: (() => {
+    try {
+      return url.getDecodedValues();
+    } catch (error) {
+      useSnackbarStore.getState().trigger({
+        message: "Invalid URL: could not read the encoded url",
+        variation: "danger",
+      });
+      //return default value
+      return { css: "", html: "", js: "" };
+    }
+  })(),
   update(key, value) {
     set((state) => {
       const newValues: EditorValues = {
